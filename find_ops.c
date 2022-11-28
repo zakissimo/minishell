ee6928080aae75156b531dfd6efae68e3794d2f4
zakissimo <zakaria.habri@gmail.com>
2022-11-28 08:49:38 +0100

    lolilol

--- find_ops.c
+++ find_ops.c
@@ -6,32 +6,43 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 19:39:54 by zhabri            #+#    #+#             */
-/*   Updated: 2022/11/28 07:45:03 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/11/28 08:35:41 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
 #include "ops.h"
 
-void	find_op(const char *input, const char *op, t_list **head, bool reset)
+void	find_op(const char *input, t_list **head)
 {
-	static int	idx;
-	char		*sub_input;
+	int			i;
+	int			j;
 	t_op		*node;
+	char		*op[6];
 
-	if (reset)
-		idx = -1;
-	sub_input = ft_strnstr(input, op, ft_strlen(input));
-	if (sub_input)
+	op[0] = "||";
+	op[1] = "<";
+	op[2] = ">>";
+	op[3] = ">";
+	op[4] = "|";
+	op[5] = NULL;
+	i = 0;
+	while (input[i])
 	{
-		idx += ft_strlen(input) - ft_strlen(sub_input) + ft_strlen(op);
-		node = malloc(sizeof(t_op));
-		node->idx = idx;
-		if (ft_strlen(op) > 1)
-			node->idx -= ft_strlen(op) - 1;
-		node->op = op;
-		ft_lstadd_back(head, ft_lstnew(node));
-		find_op(sub_input + ft_strlen(op), op, head, false);
+		j = 0;
+		while (op[j])
+		{
+			if (!ft_strncmp(input + i, op[j], ft_strlen(op[j])))
+			{
+				node = malloc(sizeof(t_op));
+				node->idx = i;
+				node->op = op[j];
+				ft_lstadd_back(head, ft_lstnew(node));
+				i += ft_strlen(op[j]);
+			}
+			j++;
+		}
+		i++;
 	}
 }
 
@@ -46,24 +57,14 @@ void	print_nodes(void *n)
 int	main(void)
 {
 	int			i;
+	int			j;
 	t_list		**head;
-	char		*op[6];
 	const char	input[] = ">>>e>cho lol ||rev|rev|rev| rev >> out > lol";
 
-	op[0] = "|";
-	op[1] = "<";
-	op[2] = ">>>";
-	op[3] = ">";
-	op[4] = "||";
-	op[5] = NULL;
 	i = 0;
 	head = malloc(sizeof(t_list *));
 	*head = NULL;
-	while (op[i])
-	{
-		find_op(input, op[i], head, true);
-		i++;
-	}
+	find_op(input, head);
 	ft_lstiter(*head, print_nodes);
 	ft_lstclear(head, free);
 	free(head);
