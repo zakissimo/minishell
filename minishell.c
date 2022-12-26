29b7ee73c7e63b664ce421120c1ab7dbb40be1b1
zakissimo <zakaria.habri@gmail.com>
2022-12-26 10:56:58 +0100

    main is normed

--- minishell.c
+++ minishell.c
@@ -6,13 +6,11 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:49:37 by zhabri            #+#    #+#             */
-/*   Updated: 2022/12/23 11:36:43 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/12/26 10:41:28 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "libft/includes/libft.h"
-#include <readline/readline.h>
 
 t_glob	*g_glob;
 
@@ -37,7 +35,6 @@ void	events(t_list **head)
 	get_ops(g_glob->input, head);
 	if (!op_error())
 	{
-		printf("New input: %s\n", g_glob->input);
 		get_args();
 		if (!pipe_error())
 		{
@@ -53,15 +50,11 @@ void	events(t_list **head)
 	free_op_list();
 }
 
-int	main(int argc, char **argv, char **envp)
+void	event_loop(void)
 {
 	t_list				**head;
 	char				*input;
 
-	init_sig_callbacks();
-	if (argc > 1)
-		init_input(argv[1]);
-	get_envp(envp);
 	while (g_glob)
 	{
 		input = readline("minishell> ");
@@ -82,5 +75,14 @@ int	main(int argc, char **argv, char **envp)
 		free(g_glob->input);
 		free(g_glob->head);
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	init_sig_callbacks();
+	if (argc > 1)
+		init_input(argv[1]);
+	get_envp(envp);
+	event_loop();
 	final_clean_up();
 }
