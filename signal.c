df883f400f45b2f7ae555af356e03043468e8477
zakissimo <zakaria.habri@gmail.com>
2022-12-27 11:17:06 +0100

    heredoc sigint to do

--- signal.c
+++ signal.c
@@ -6,7 +6,7 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 10:32:29 by zhabri            #+#    #+#             */
-/*   Updated: 2022/12/26 15:53:28 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/12/27 11:13:56 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ static void	handler(int sig)
 	if (sig == SIGINT && !g_glob->in_child)
 	{
 		ft_putchar_fd('\n', 1);
-		ft_putstr_fd("minishell> ", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 		g_glob->exit_ret = 130;
 	}
 	else if (sig == SIGINT && g_glob->in_child)
